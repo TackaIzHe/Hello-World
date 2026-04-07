686b8328074f4f39442d81e254679bbcd30c3783
TackaIzHe <144840948+TackaIzHe@users.noreply.github.com>
2026-04-07 17:00:58 +0000

    asd

--- task3/app/Http/Controllers/UserController.php
+++ task3/app/Http/Controllers/UserController.php
@@ -22,12 +22,12 @@ class UserController extends Controller
     public function city($name)
     {
         $users = [
-'user1' => 'city1',
-'user2' => 'city2',
-'user3' => 'city3',
-'user4' => 'city4',
-'user5' => 'city5',
-];
+    'user1' => 'city1',
+    'user2' => 'city2',
+    'user3' => 'city3',
+    'user4' => 'city4',
+    'user5' => 'city5',
+    ];
         return $users[$name];
     }
 }
